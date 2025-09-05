0c162a744a2e72ca3f8145f2e11277f33e9a567f
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-05 16:12:46 +0300

    12. EditableSpan.stories.tsx was added

--- src/AddItemForm.stories.tsx
+++ src/AddItemForm.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
-
+import { action } from 'storybook/actions';
 import { AddItemForm } from './AddItemForm';
 
 const meta = {
@@ -8,11 +8,18 @@ const meta = {
 } satisfies Meta<typeof AddItemForm>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
-export const Default: Story = {
-    args: {
-        addItem: (title: string) => {},
-    }
-};
 
+// type Story = StoryObj<typeof meta>;
+
+// export const Default: Story = {
+//     args: {
+//         addItem: (title: string) => {},
+//     }
+// };
+
+const callback = action('Button add was pressed inside the form')
+
+export const AddItemFormBaseExample = () => {
+    return <AddItemForm addItem={callback} />
+}
 
